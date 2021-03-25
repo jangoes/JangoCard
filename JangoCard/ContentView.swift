9dbc145768d5e0a862090d563be90e121265a022
John Ellie Go <ellie.go12@gmail.com>
2021-03-25 15:15:56 +0800

    JangoCard SwiftUI

--- JangoCard/ContentView.swift
+++ JangoCard/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 22/255, green: 160/255, blue: 133/255)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("John Ellie Go")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+63 9123 456 789", imageName: "phone.fill")
+                InfoView(text: "jango@gmail.com", imageName: "envelope.fill")
+            }
+        }
+
     }
 }
 
